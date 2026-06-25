31e5df34f3c1b0bcc92dbefd3af91f5e695b1468
hayashi-workshop <hayashi@mech.kobe-u.ac.jp>
2026-06-25 09:25:49 +0900

    SoA allocation corrected in lbm_lib.py

--- lb_solver/lbm_lib.py
+++ lb_solver/lbm_lib.py
@@ -27,7 +27,8 @@ class lbm_skelton:
         self,
         nd,  # domain size (nx, ny) or (nx, ny, nz)
         model_config,
-        omega1=None # relaxation parameter for shear (omega_{1})
+        omega1=None, # relaxation parameter for shear (omega_{1})
+        SoA = True # default SoA layout; False->AoS
     ):
         self.nd = nd
         self.dim = len(nd)
@@ -43,18 +44,28 @@ class lbm_skelton:
             self.omega[i] = 1.0
         self.omega[1] = 1.0 if omega1 == None else omega1
         self.rho = ti.field(float, shape=self.nd)
-        # - - - - - SoA memory layout - - - - - - - - - #
-        self.ti_axes = ti.ij if self.dim == 2 else ti.ijk
-        self.vel = ti.Vector.field(self.dim, float)
-        for d in range(self.dim):
-            ti.root.dense(self.ti_axes, self.nd).place(self.vel.get_scalar_field(d))
-        self.f_old = ti.Vector.field(self.Q, float)
-        self.f_new = ti.Vector.field(self.Q, float)
-        block = ti.root.dense(self.ti_axes, self.nd)
-        for q in range(self.Q): 
-            block.place(self.f_old.get_scalar_field(q))
-        for q in range(self.Q): # allocation must be done with another for loop
-            block.place(self.f_new.get_scalar_field(q))
+        if SoA:
+            # - - - - - SoA memory layout - - - - - - - - - #
+            self.ti_axes = ti.ij if self.dim == 2 else ti.ijk
+            self.vel = ti.Vector.field(self.dim, float)
+            for d in range(self.dim):
+                ti.root.dense(self.ti_axes, self.nd).place(self.vel.get_scalar_field(d))
+
+            self.f_old = ti.Vector.field(self.Q, float)
+            self.f_new = ti.Vector.field(self.Q, float)
+
+            block_old = ti.root.dense(self.ti_axes, self.nd)
+            for q in range(self.Q): 
+                block_old.place(self.f_old.get_scalar_field(q))
+
+            block_new = ti.root.dense(self.ti_axes, self.nd)
+            for q in range(self.Q): # allocation must be done with another for loop
+                block_new.place(self.f_new.get_scalar_field(q))
+        else:
+            # - - - - - AoS memory layout - - - - - - - - - #
+            self.vel   = ti.Vector.field(self.dim, float, shape=self.nd)
+            self.f_old = ti.Vector.field(self.Q, float, shape=self.nd)
+            self.f_new = ti.Vector.field(self.Q, float, shape=self.nd)
         # - - - - - - - - - - - - - - - - - - - - - - - #
         self.mask = ti.field(float, shape=self.nd)
         # - - - - - - - - - - - - - - - - - - - - - - - #
